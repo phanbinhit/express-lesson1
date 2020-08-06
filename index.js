e4dd497cf6d867090b62d600269ec0d3d45bb05b
phanbinh <phanbinh2671999@gmail.com>
2020-08-06 15:57:48 +0700

    Teamplate-engine-06.08.2020

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views')
+
 app.get('/', function(req, res) {
-	res.send('Hello');
+	res.render('index', {name: 'CoderX'});
+});
+
+app.get('/user', function(req, res) {
+	res.render('users/user', {users: [
+		{id: 1, name: 'Binh'},
+		{id: 2, name: 'Hoang'}
+	]});
 });
 
 app.listen(port, function() {
